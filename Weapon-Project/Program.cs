69eecf2c54f1c2b2ab719999e4e924e70a9ccc73
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2021-10-10 11:05:59 +0400

    First commit

---
+++ Weapon-Project/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AvtomatProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon silah = new Weapon();
+            string start;
+            Console.WriteLine("        Avtomat Simulyasiyasi");
+            Console.WriteLine("********************************************");
+            Console.WriteLine("1-Atish   2-Magazin Deyish     3-Magazin Doldur");
+            Console.WriteLine("Bashlamaq ucun 1-e basin.");
+            start = Console.ReadLine();
+            Console.Clear();
+            while (silah.gulle >= 1)
+            {
+                if (start == "1")
+                {
+                    Console.WriteLine("Atish novu daxil edin: Tek-tek atmaq ucun 't', avtomatik atmaq ucun 'a' yazin.");
+                    silah.Bashla(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine($"Gulle sayi:{silah.gulle}");
+                    if (silah.gulle == 0)
+                    {
+                        Console.WriteLine("Gulle bitti.");
+                        Console.WriteLine("Magazini deyismek ucun 2-e basin.");
+                        silah.Bashla(Console.ReadLine());
+                    }
+                    else if (silah.gulle >= 30)
+                    {
+                        Console.WriteLine("Magazin tam doldu.Yeniden baslamaq ucun herhansi bir klavise basin. ");
+                        silah.Bashla(Console.ReadLine());
+                        Console.Clear();
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ Weapon-Project/Weapon.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvtomatProject
+{
+    class Weapon
+    {
+        public int gulle { get; private set; }
+        public Weapon()
+        {
+            gulle = 30;
+        }
+        public int Bashla(string atishNovu)
+        {
+            if (atishNovu == "t")
+            {
+                gulle--;
+            }
+            else if (atishNovu == "a")
+            {
+                gulle = 0;
+            }
+            else if (atishNovu == "2")
+            {
+                MagazinDeyish();
+            }
+            else if (atishNovu == "3" && gulle < 30)
+            {
+                MagazinDoldur();
+            }
+            return gulle;
+        }
+        public void MagazinDeyish()
+        {
+            gulle = 30;
+            Console.WriteLine($"Gulle sayi:{gulle}");
+        }
+
+        public int MagazinDoldur()
+        {
+            gulle++;
+            return gulle;
+        }
+    }
+}
